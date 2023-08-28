7ebe3229c3a974d0eff33d91a5852fda75070c44
Heo <gjgudrn1114@naver.com>
2023-08-28 11:34:24 +0900

    SSE 프로토콜 적용 Controller 작성

---
+++ src/main/java/com/cos/chatapp/ChatController.java
@@ -0,0 +1,30 @@
+package com.cos.chatapp;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@RestController
+public class ChatController {
+    private final ChatRepository chatRepository;
+
+    // SSE 프로토콜
+    @GetMapping(value="/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver) {
+        return chatRepository.mFindBySender(sender,receiver)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    // Mono - 데이터를 한 번만 리턴받는다. 데이터 확인 차 삽입
+    @PostMapping("/chat")
+    public Mono<Chat> setMsg(@RequestBody Chat chat) {
+        chat.setCreatedAt(LocalDateTime.now());
+        return chatRepository.save(chat);
+    }
+}
